b17fdfe2bc4e4d48e9f85e7948da5efc7c3cfeeb
anchalladhar <133622298+anchalladhar@users.noreply.github.com>
2023-07-22 20:48:55 -0400

    Delete ViewModels directory

--- ViewModels/RunningClubByCity.cs
+++
@@ -1,9 +0,0 @@
-﻿using RunGroopWebApp.Models;
-
-namespace RunGroopWebApp.ViewModels
-{
-    public class RunningClubByCity
-    {
-        public List<City> Cities { get; set; } = null;
-    }
-}
--- ViewModels/RunningClubByState.cs
+++
@@ -1,9 +0,0 @@
-﻿using RunGroopWebApp.Models;
-
-namespace RunGroopWebApp.ViewModels
-{
-    public class RunningClubByState
-    {
-        public List<State> States { get; set; } = null;
-    }
-}
